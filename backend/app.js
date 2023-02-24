b8a1b1a1f5a6dba7e0eda8ba795b8bc94bc1d45a
nikosapostolakis <ni19852011@gmail.com>
2023-02-25 00:26:26 +0200

    Error Handling

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const epxress = require('express');
+const bodyParser = require('body-parser');
+
+const placesRoutes = require('./routes/places-routes');
+
+const app = epxress();
+
+app.use('/api/places', placesRoutes);
+
+app.use((error, req,res,next)=>{
+    if(res.headerSent){
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({message: error.message || 'Unknown Error'});
+});
+app.listen(5000);
